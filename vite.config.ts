db80a682901ae9bfaa38a96a6d75d59a6b152447
Athipannawat <theacontacts@gmail.com>
2024-11-29 23:14:37 +0700

    update Config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+/*
 export default defineConfig({
   plugins: [react()],
   resolve:{
     alias: [{ find: '@', replacement: '/src' }],
   },
+})
+*/
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
